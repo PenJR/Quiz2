7518a6b00592ca460ce7e2cdddf7035ba488a420
PenJR <pinatacan.j@gmail.com>
2025-09-29 10:57:27 +0800

    a

---
+++ Quiz2/app/src/main/java/com/example/quiz2/bmi.java
@@ -0,0 +1,67 @@
+package com.example.quiz2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class bmi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myButtonListenerMethod();
+    }
+
+    public void myButtonListenerMethod() {
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText heightText = findViewById(R.id.heightInput);
+                EditText weightText = findViewById(R.id.weightInput);
+                EditText BMIResult = findViewById(R.id.BMIResult);
+                TextView BMICategory = findViewById(R.id.BMICategory);
+
+                String heightStr = heightText.getText().toString();
+                String weightStr = weightText.getText().toString();
+
+                double height = Double.parseDouble(heightStr);
+                double weight = Double.parseDouble(weightStr);
+                double BMI = (weight)/(height * height);
+
+                DecimalFormat df = new DecimalFormat("#.#");
+                double BMI_trimmed = Double.parseDouble(df.format(BMI));
+
+                BMIResult.setText(Double.toString(BMI_trimmed));
+
+                String BMI_Cat;
+                if (BMI < 15)
+                    BMI_Cat = "Very severely underweight";
+                else if (BMI < 16)
+                    BMI_Cat = "Severely underweight";
+                else if (BMI < 18.5)
+                    BMI_Cat = "Underweight";
+                else if (BMI < 25)
+                    BMI_Cat = "Normal";
+                else if (BMI < 30)
+                    BMI_Cat = "Overweight";
+                else if (BMI < 35)
+                    BMI_Cat = "Obese Class 1 – Moderately Obese";
+                else if (BMI < 40)
+                    BMI_Cat = "Obese Class 2 - Severely Obese";
+                else
+                    BMI_Cat = "Obese Class 3 - Very Severely Obese";
+
+                BMICategory.setText(BMI_Cat);
+            }
+        });
+    }
+
+}
